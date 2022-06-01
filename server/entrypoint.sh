6e0555f740763300cec46b5ed79c576a5c3b925f
Daniel Nazarian <danielnazarian@outlook.com>
2022-06-01 13:42:00 -0400

    server files

---
+++ server/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#
+# Used for deployments and development to run the server
+#
+
+until cd /app/server
+do
+    echo "Waiting for server volume..."
+done
+
+until ./manage.py migrate
+do
+    echo "Waiting for db to be ready..."
+    sleep 2
+done
+
+./manage.py collectstatic --noinput
+
+gunicorn dansbackend.wsgi:application --bind 0.0.0.0:8000 --workers 4 --threads 4 --log-file=-
